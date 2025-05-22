23fb91b52dc5f383b828afad61a5dc60333e3b95
AfterRain007 <afterrain007@gmail.com>
2025-05-22 23:27:42 +0700

    Hell yeah

--- public/script.js
+++ public/script.js
@@ -1,2 +1,12 @@
 const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
 const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
+
+// JavaScript to detect scroll and apply the navbar-scrolled class
+window.addEventListener('scroll', function () {
+const navbar = document.querySelector('.navbar');
+if (window.scrollY > 25) {
+        navbar.classList.add('navbar-scrolled');
+    } else {
+        navbar.classList.remove('navbar-scrolled');
+    }
+});
