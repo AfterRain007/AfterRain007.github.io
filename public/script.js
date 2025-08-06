5840283d4165250d9b7986459a1cc3ca4c700a79
AfterRain007 <afterrain007@gmail.com>
2025-08-06 23:33:35 +0700

    Boom

--- public/script.js
+++ public/script.js
@@ -10,3 +10,13 @@ if (window.scrollY > 25) {
         navbar.classList.remove('navbar-scrolled');
     }
 });
+
+$(document).ready(function () {
+  // Check for dark mode
+  const isDarkMode = window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches;
+
+  // If dark mode and the table exists, add class
+  if (isDarkMode && $('table.table.table-bordered').length) {
+    $('table.table.table-bordered').addClass('table-dark');
+  }
+});
